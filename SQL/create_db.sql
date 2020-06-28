a551d64fed72bcf647640b0c2be11551c8447f20
gmstro <67560487+gmstro@users.noreply.github.com>
2020-06-28 21:56:31 +0100

    Update create_db.sql

--- SQL/create_db.sql
+++ SQL/create_db.sql
@@ -1,3 +1,16 @@
 DROP DATABASE IF EXISTS interview;
 CREATE DATABASE interview;
 USE interview;
+
+CREATE TABLE bets (
+	id int,
+	numbets int,
+	game varchar(255),
+	stake decimal(7,2),
+	returns decimal(7,2),
+	clientid int,
+	date date
+);
+
+CREATE INDEX idx_id
+ON bets (id);
